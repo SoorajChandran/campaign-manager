5d84e4cc59b6feb9198359ae1e1111e368a20aa4
Sooraj Chandran <soorajchn@gmail.com>
2017-11-30 13:53:42 +0530

    Splitting campaign page into components

--- src/components/pages/Campaign.js
+++ src/components/pages/Campaign.js
@@ -1,21 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Sidebar from "../ui-elements/Sidebar";
 import Header from "../ui-elements/Header";
 import Funnel from "../ui-elements/Funnel";
+import CampaignData from "../ui-elements/CampaignData";
+import CampaignMetric from "../ui-elements/CampaignMetric";
 import '../../App.css'
 
 class Campaign extends  React.Component {
-
-  constructor(props){
-    super(props);
-  }
-
 
   getData = (element) => {
     return element.id === this.props.match.params.id;
   }
 
-  addGoals = (e) => {
+  addGoals = () => {
     const elem =  document.getElementById("goalValue");
     const goalValue = elem.options[elem.selectedIndex].value;
     const data = this.props.data;
@@ -58,75 +55,25 @@ class Campaign extends  React.Component {
         <div className="main">
         <div className="row">
           <div className="col-sm-8 mt-4">
-          <div className="campaignCard">
-          <h3 className="campaignTitle mb-1 mt-4 text-center"> {campaignData.title}</h3>
-            <div className="campaignMetric text-center pt-4 pb-4">
-             <Funnel {...campaignData} />
-            </div>
-            <div className="col-sm-12 campaignMetric bt1 mt-3">
-              <div className="row">
-              <div className="col-sm-3 br1 pt-4">
-                <div className="muted metricTitle"> Created at </div>
-                <div className="metricValue"> { Date(campaignData.created_at)} </div>
+            <div className="campaignCard">
+            <h3 className="campaignTitle mb-1 mt-4 text-center"> {campaignData.title}</h3>
+              <div className="campaignMetric text-center pt-4 pb-4">
+               <Funnel {...campaignData} />
               </div>
-              <div className="col-sm-3 br1 pt-4">
-                <div className="muted metricTitle"> Start date </div>
-                <div className="metricValue"> {campaignData.start_date}  </div>
-                
-              </div>
-              <div className="col-sm-3 br1 pt-4">
-                <div className="muted metricTitle">End date </div>
-                <div className="metricValue"> {campaignData.end_date} </div>
-              </div>
-              <div className="col-sm-3 pt-4">
-                <div className="muted metricTitle">Duration </div>
-                <div className="metricValue"> {campaignData.days} days </div>
-              </div>
+              <div className="col-sm-12 campaignMetric bt1 mt-3">
+                <CampaignData {...campaignData} />
               </div>
             </div>
-            
-          </div>
           </div>
           <div className="col-sm-4 mt-4">
-          <div className="campaignCard">
-          <div className="col-sm-12 campaignMetric pt-3">
-              
-              <div className="">
-                <div className="muted metricTitle"> Lead conversion amount </div>
-                <div className="metricValue large">
-
-                  <span className="sign">$</span>
-                  <span className="value"> { campaignData.lead_conversion_amount || ' - '} </span>
-                 </div>
-              </div>
-              <div className="">
-                <div className="muted metricTitle"> Lead conversion value </div>
-                <div className="metricValue large"> 
-                <span className="sign">$</span>
-                 <span className="value"> {campaignData.lead_conversion_value_cents/100 || ' - '} </span>
-                </div>  
-              </div>
-
-              <div className="">
-                <div className="muted metricTitle"> Sales conversion amount </div>
-                <div className="metricValue large"> 
-                <span className="sign">$</span>
-                 <span className="value"> {campaignData.sales_conversion_amount || ' - '} </span>
-                </div>  
-              </div>
-              <div className="">
-                <div className="muted metricTitle"> Sales conversion value </div>
-                <div className="metricValue large"> 
-                <span className="sign">$</span>
-                 <span className="value"> {campaignData.sales_conversion_value_cents/100 || ' - '} </span>
-                </div> 
+            <div className="campaignCard">
+            <div className="col-sm-12 campaignMetric pt-3">
+                <CampaignMetric {...campaignData} /> 
+            </div>
+              <div className="col-sm-12 pb-4">
+                { this.renderGoals(campaignData.goal) }
               </div>
             </div>
-            <div className="col-sm-12 pb-4">
-              { this.renderGoals(campaignData.goal) }
-            </div>
-            
-          </div>
           </div>
           
         </div>
---
+++ src/components/ui-elements/CampaignMetric.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const CampaignMetric = (data) => (
+      <div>
+        <div>
+          <div className="muted metricTitle"> Lead conversion amount </div>
+          <div className="metricValue large">
+            <span className="sign">$</span>
+            <span className="value"> { data.lead_conversion_amount || ' - '} </span>
+           </div>
+        </div>
+        <div>
+          <div className="muted metricTitle"> Lead conversion value </div>
+          <div className="metricValue large"> 
+          <span className="sign">$</span>
+           <span className="value"> {data.lead_conversion_value_cents/100 || ' - '} </span>
+          </div>  
+        </div>
+
+        <div>
+          <div className="muted metricTitle"> Sales conversion amount </div>
+          <div className="metricValue large"> 
+          <span className="sign">$</span>
+           <span className="value"> {data.sales_conversion_amount || ' - '} </span>
+          </div>  
+        </div>
+        <div>
+          <div className="muted metricTitle"> Sales conversion value </div>
+          <div className="metricValue large"> 
+          <span className="sign">$</span>
+           <span className="value"> {data.sales_conversion_value_cents/100 || ' - '} </span>
+          </div> 
+        </div>
+      </div>
+)
+
+export default CampaignMetric;
